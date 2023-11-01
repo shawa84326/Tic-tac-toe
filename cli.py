1b2e3567ebfad7eafe9a7a48257548620604cd76
shawa84326 <148395690+shawa84326@users.noreply.github.com>
2023-10-31 23:44:09 -0700

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,19 @@
+# This file contains the Command Line Interface (CLI) for
+# the Tic-Tac-Toe game. This is where input and output happens.
+# For core game logic, see logic.py.
+
+from logic import make_empty_board
+
+
+# Reminder to check all the tests
+
+if __name__ == '__main__':
+    board = make_empty_board()
+    winner = None
+    while winner == None:
+        print("TODO: take a turn!")
+        # TODO: Show the board to the user.
+        # TODO: Input a move from the player.
+        # TODO: Update the board.
+        # TODO: Update who's turn it is.
+        winner = 'X'  # FIXME
