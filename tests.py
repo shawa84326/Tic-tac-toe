256ad1f50f4e6956f2256833a10e9e0243591c78
shawa84326 <148395690+shawa84326@users.noreply.github.com>
2023-11-25 19:02:55 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,19 +1,49 @@
 import unittest
-import logic
+from logic import make_empty_board, get_winner, other_player
 
+class TicTacToeTests(unittest.TestCase):
+    def test_make_empty_board(self):
+        board = make_empty_board()
+        self.assertEqual(len(board), 3)
+        for row in board:
+            self.assertEqual(len(row), 3)
+            self.assertTrue(all(cell is None for cell in row))
 
-class TestLogic(unittest.TestCase):
+    def test_other_player(self):
+        self.assertEqual(other_player('X'), 'O')
+        self.assertEqual(other_player('O'), 'X')
 
-    def test_get_winner(self):
+    def test_get_winner_no_winner(self):
         board = [
-            ['X', None, 'O'],
-            [None, 'X', None],
-            [None, 'O', 'X'],
+            ['X', 'O', 'X'],
+            ['O', 'X', 'O'],
+            ['O', 'X', 'O'],
         ]
-        self.assertEqual(logic.get_winner(board), 'X')
+        self.assertIsNone(get_winner(board))
 
-    # TODO: Test all functions from logic.py!
+    def test_get_winner_horizontal(self):
+        board = [
+            ['X', 'X', 'X'],
+            ['O', 'O', None],
+            [None, None, None],
+        ]
+        self.assertEqual(get_winner(board), 'X')
 
+    def test_get_winner_vertical(self):
+        board = [
+            ['O', 'X', None],
+            ['O', 'X', None],
+            ['O', None, None],
+        ]
+        self.assertEqual(get_winner(board), 'O')
+
+    def test_get_winner_diagonal(self):
+        board = [
+            ['X', 'O', None],
+            ['O', 'X', None],
+            [None, None, 'X'],
+        ]
+        self.assertEqual(get_winner(board), 'X')
 
 if __name__ == '__main__':
     unittest.main()
