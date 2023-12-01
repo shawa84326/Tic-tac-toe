5964bf740bb144f7746f2a546997f8b6232121be
shawa84326 <148395690+shawa84326@users.noreply.github.com>
2023-11-30 17:30:04 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,10 +1,18 @@
 from logic import make_empty_board, get_winner, other_player
+import csv
+import os
+
+LOGS_DIR = "logs"
+LOG_FILE = os.path.join(LOGS_DIR, "database.csv")
 
 def print_board(board):
     for row in board:
         print(" | ".join([cell if cell else " " for cell in row]))
         print("-" * 9)
-
+def record_winner(player):
+    with open(LOG_FILE, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([player])
 def get_user_move():
     while True:
         try:
@@ -15,6 +23,7 @@ def get_user_move():
             print("Invalid input. Please enter in the format 'row,col'.")
 
 def main():
+    
     board = make_empty_board()
     player = 'X'
     winner = None
@@ -33,9 +42,11 @@ def main():
         winner = get_winner(board)
         
         if winner:
+            record_winner(winner)
             print_board(board)
             print(f"Player {winner} wins!")
         elif all(all(cell for cell in row) for row in board):
+            record_winner("Draw")
             print_board(board)
             print("It's a draw!")
             break
